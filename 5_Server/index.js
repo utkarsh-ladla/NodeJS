91d726ad16f882ff18b65dce67971f677b9a992d
UTKARSH LADLA <128512468+utkarsh-ladla@users.noreply.github.com>
2024-10-25 23:26:34 +0530

    About url

--- 5_Server/index.js
+++ 5_Server/index.js
@@ -2,32 +2,39 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 //creating a web server
-const myServer = http.createServer((req, res)=> {
-    // console.log(req);
-    console.log(req.headers);  //headers is usedfor getting extra info of client system and info
-    //creatig a log where it store some data 
-    const log = `${Date.now()}: ${req.url} New Req Received \n`;
-    fs.appendFile('log.txt', log, (err,data)=> {
-      //we append the log in txt file 
-      switch (req.url) {
-        case "/":
-            res.end("HomePage");
-            break;
-            case "/about":
-            res.end("I am Utkarsh Ladla");
-            break;
-            default:
-            res.end("404 Not found ");
-            break;
-      }
-    })
+const myServer = http.createServer((req, res) => {
+  if(req.url === '/favicon.ico') return res.end();
+
+  // console.log(req);
+  console.log(req.headers);  //headers is usedfor getting extra info of client system and info
+  //creatig a log where it store some data 
+  const log = `${Date.now()}: ${req.url} New Req Received \n`;
+  const myUrl = url.parse(req.url, true); //query parameter string 
+  console.log(myUrl);
+ 
+  fs.appendFile('log.txt', log, (err, data) => {
+    //we append the log in txt file 
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const username = myUrl.query.myname
+        res.end(`I am ${username}`);  //paersig username from query parameter 
+        break;
+      default:
+        res.end("404 Not found");
+        break;
+    }
+  })
 });
 //arrow function passed in argument of createserver function is taking incoming request....
 //that callback fun have to parameter req and res
 
 //for creaating a server which have to serve in any given port 
 myServer.listen(8000, () => {
-    console.log("server started");
+  console.log("server started");
 });
